02c50cd46ab18996e50c51754f3a6051cbe04c1a
Yuusuke Hasegawa <yuusuke.hasehase@gmail.com>
2020-06-07 13:27:16 +0900

    feat: rename package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FirebaseCodable",
-            targets: ["firebase-codable"]),
+            targets: ["FirebaseCodable"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,12 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "firebase-codable",
+            name: "FirebaseCodable",
             dependencies: []
         ),
         .testTarget(
-            name: "firebase-codableTests",
-            dependencies: ["firebase-codable"]),
+            name: "FirebaseCodableTests",
+            dependencies: ["FirebaseCodable"]),
     ],
     swiftLanguageVersions: [.v5]
 )
