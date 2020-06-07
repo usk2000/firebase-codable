184bff12dcabd19ffc1190e194c04236d6b2c16b
Yuusuke Hasegawa <yuusuke.hasehase@gmail.com>
2020-06-07 15:32:26 +0900

    feat: add framework setting

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FirebaseCodable",
-            dependencies: []
+            dependencies: [],
+            linkerSettings: [
+                .linkedFramework("FirebaseFirestore")
+            ]
         ),
         .testTarget(
             name: "FirebaseCodableTests",
